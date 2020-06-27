e72e8267f5d8150d46ffca43d8ad09c35930c0b0
赵毓文 <zhaoyuwen@zhaoyuwendeMacBook-Pro.local>
2020-06-27 21:57:03 +0800

    代办事项 子组件优化

---
+++ src/components/Todo.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="todo">
+    <el-table :data="tableData" style="width: 100%">
+      <el-table-column label="序号" type="index" width="180"> </el-table-column>
+      <el-table-column label="事项" width="180">
+        <template slot-scope="scope">
+          <el-popover trigger="hover" placement="top">
+            <p>备注: {{ scope.row.todo }}</p>
+            <!-- <p>住址: {{ scope.row.address }}</p> -->
+            <div slot="reference" class="name-wrapper">
+              <el-tag size="medium">{{ scope.row.todo }}</el-tag>
+            </div>
+          </el-popover>
+        </template>
+      </el-table-column>
+      <el-table-column label="操作">
+        <template slot-scope="scope">
+          <el-button v-if="scope.row.type === '1'" size="mini" type="success" @click="handleEdit(scope.$index, scope.row)">移至日后代办</el-button>
+          <el-button v-if="scope.row.type === '2'" size="mini" type="success" @click="handleEdit(scope.$index, scope.row)">移至今日代办</el-button>
+          <el-button size="mini" type="danger" @click="handleDelete(scope.$index, scope.row)">删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+  </div>
+</template>
+
+<script>
+// type:{
+//     '1': 今日代办,
+//     '2': 日后代办,
+// }
+export default {
+  props: ['type'],
+  data() {
+    return {
+      tableData: [
+        {
+          todo: 'docker',
+          type: '1',
+          status: '1'
+        }
+      ]
+    }
+  },
+  methods: {
+    handleEdit(index, row) {
+      console.log(index, row)
+    },
+    handleDelete(index, row) {
+      console.log(index, row)
+    }
+  }
+}
+</script>
--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <el-container style="height: 500px; border: 1px solid #eee">
+    <el-container style="border: 1px solid #eee">
       <el-aside width="200px" style="background-color: rgb(238, 241, 246)">
         <Nav />
       </el-aside>
@@ -15,8 +15,20 @@
                 <el-option label="今日代办" value="1"></el-option>
                 <el-option label="日后代办" value="2"></el-option>
               </el-select>
-              <el-button type="primary" slot="append" icon="el-icon-edit"></el-button>
+              <el-button type="primary" slot="append" icon="el-icon-edit" @click="submit"></el-button>
             </el-input>
+          </div>
+
+          <h3>今日代办</h3>
+
+          <div style="margin-top: 15px;">
+            <Todo v-bind:type="1" />
+          </div>
+
+          <h3>日后代办</h3>
+
+          <div style="margin-top: 15px;">
+            <Todo v-bind:type="2"/>
           </div>
         </el-main>
       </el-container>
@@ -51,34 +63,33 @@
 // 4.代办事项可以转换状态
 // 5.可以标记删除
 //
-import axios from 'axios'
+// import axios from 'axios'
 import Nav from '@/components/Nav.vue'
+import Todo from '@/components/Todo.vue'
 
 // .可以显示常用的快捷键，并进行复制
 export default {
   data() {
     return {
       todo: 'docker',
-      todoType: '1'
+      todoType: '1',
+      todayTodos: [
+        {
+          todo: 'docker',
+          type: '1',
+          status: '1'
+        }
+      ],
+      afterTodos: []
     }
   },
   components: {
-    Nav
+    Nav,
+    Todo
   },
   methods: {
-    getRandom() {
-      this.randomNumber = this.getRandomFromBackend()
-    },
-    getRandomFromBackend() {
-      const path = 'http://127.0.0.1:5000/home/'
-      axios
-        .get(path)
-        .then(response => {
-          this.tableData = response.data.data
-        })
-        .catch(error => {
-          console.log(error)
-        })
+    submit() {
+      console.log('123')
     }
   },
   created() {
